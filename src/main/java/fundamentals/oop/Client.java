5fd08c0756bfcb55f6a2d3ff6af36be1bd30b0d6
Dorina Gheorghe <dorinut_g@yahoo.com>
2021-01-31 15:47:41 +0200

    bank account exercise

--- src/main/java/fundamentals/oop/Client.java
+++ src/main/java/fundamentals/oop/Client.java
@@ -1,5 +1,7 @@
 package fundamentals.oop;
 
+import java.util.Arrays;
+
 /**
  * Client  which consists of:
  * constructor: with arguments: name and cnp (for initializing the fields and the  bankingAccount array will be
@@ -48,11 +50,21 @@ public class Client {
         this.bankAccount = new BankAccount();
     }
 
-    public String toString() { //creere metoda prin care in main prin System.out.println(client)/
+    //public String toString() { //creere metoda prin care in main prin System.out.println(client)/
         // System.out.println(client.toString()) afiseaza ce este in metoda to string (nume, cnp, banckAccount;
-        return "Name= " + this.name + ", CNP=" + this.cnp + ", Bank account= " + this.bankAccount;
+       // return "Name= " + this.name + ", CNP=" + this.cnp + ", Bank account= " + this.bankAccount;
 
-    }// cu Alt+Ins se poate insera toString si suprascrie metoda de mai sus
+    //}// cu Alt+Ins se poate insera toString si suprascrie metoda de mai sus
+
+    @Override
+    public String toString() {
+        return "Client{" +
+                "name='" + name + '\'' +
+                ", cnp='" + cnp + '\'' +
+                ", bankAccount=" + bankAccount +
+                ", bankAccounts=" + Arrays.toString(bankAccounts) +
+                '}';
+    }
 
     public String getName() {
         return name;
