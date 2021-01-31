41d2a315e5e7a62b86f074ea411bd1df0bcd8738
Dorina Gheorghe <dorinut_g@yahoo.com>
2021-01-31 15:44:35 +0200

    bank account exercise

---
+++ src/main/java/fundamentals/oop/BankAccount.java
@@ -0,0 +1,109 @@
+package fundamentals.oop;
+
+/**
+ * Write an application that contains the following classes:
+ * BankAccount which is consists of:
+ * constructor: with arguments: type and currency (for initializing the fields and the  balance will be initialized
+ * with 0 in the constructor)
+ * fields: type of the account (SAVING or SPENDING),  balance (the amount of money), currency (USD, EUR),
+ * methods:
+ * addMoney (adds money to the balance, - if the account is of type SPENDING the amount of money that is added
+ * needs to be bigger than 10% of balance otherwise print a suggestive  error message)
+ * withdrawMoney ( withdraws  money from the balance - if the account is of type SAVING the remaining balance can
+ * not be < 0 otherwise print a suggestive  error message ),
+ * covertMoneyToCurrency (that takes 2 arrguments: amountOfMoney to convert, and the currency to convert to and returns the result = the money in the input currency)
+ */
+public class BankAccount {
+    /**
+     * Type of the account: SAVING or SPENDING
+     */
+    private String type;
+    /**
+     * Currency: USD or EUR
+     */
+    private String currency;
+    /**
+     * Balance of the account
+     */
+    public static final double USD_TO_EUR = 0.85;
+    public static final double EUR_TO_USD = 1.25;
+
+    private double balance;
+
+    public BankAccount(String type, String currency) {// 1fst constructor creere constructor
+        // si parametri
+        this.type = type;
+        this.currency = currency;
+        this.balance = 0;
+    }
+
+    public BankAccount() {              //// 2nd constructor
+        this.type = "SAVING";
+        this.currency = "USD";
+        this.balance = 0;
+    }
+
+    //* addMoney (adds money to the balance, - if the account is of type SPENDING the amount of money that is added
+// * needs to be bigger than 10% of balance otherwise print a suggestive  error message)
+    public void addMoney(double newMoney) {
+        if (this.type == "SPENDING" && newMoney <= this.balance / 10) {
+            System.out.println("Please add at least 10% of your actual balance");
+            return;
+        }
+        this.balance += newMoney;
+        System.out.println("Your new balance is: " + this.balance);
+    }
+
+    //* withdrawMoney ( withdraws  money from the balance - if the account is of type SAVING the remaining
+    // balance can not be < 0 otherwise print a suggestive  error message )
+    public void withdrawMoney(double newMoney) {
+        if (this.type == "SAVING" && this.balance - newMoney < 0) {
+            System.out.println("Unsufficient founds");
+            return;
+        }
+        this.balance -= newMoney;
+        System.out.println("Your new balance is: " + this.balance);
+    }
+
+    //* covertMoneyToCurrency (that takes 2 arrguments: amountOfMoney to convert, and the currency to convert
+    // to and returns the result = the money in the input currency)
+    public static double convertMoneyToCurrency(double amountOfMoney, String inputCurrency, String outputCurrency) {
+        if (inputCurrency.equals("EUR") && outputCurrency.equals("USD")) {
+            return amountOfMoney * EUR_TO_USD;
+        }
+        return amountOfMoney * USD_TO_EUR;
+    }
+
+    @Override
+    public String toString() {
+        return "BankAccount{" +
+                "type='" + type + '\'' +
+                ", currency='" + currency + '\'' +
+                ", balance=" + balance +
+                '}';
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getCurrency() { //CTRL+Click ne duce direct la metoda
+        return currency;
+    }
+
+    public void setCurrency(String currency) {
+        this.currency = currency;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+}
---
+++ src/main/java/fundamentals/oop/Client.java
@@ -0,0 +1,80 @@
+package fundamentals.oop;
+
+/**
+ * Client  which consists of:
+ * constructor: with arguments: name and cnp (for initializing the fields and the  bankingAccount array will be
+ * initialized with an empty array or only instantiated)
+ * fields: bankingAccounts ( array of max 2 banking accounts), name, cnp
+ * methods:
+ * addBankAccount(takes one argument, bankAccount obj - if there already exists a banking account of the same type
+ * in the array an error message is printed)
+ * getSavingAccount( returns the bankAccount obj of type SAVING from the array, if there isn’t a saving account a
+ * null object is return along with printing an error message)
+ * removeAccountByType (takes a string argument which represents the type of the account to be removed from the
+ * array and prints a success message if the operations succeeded or error message if not - where the account did
+ * not exist in the array)
+ * Main class that contains the main method where all the methods are invoked/tested with values inserted by you
+ */
+
+public class Client {
+
+    private String name;//tip string, nume field: name
+    private String cnp;
+    private BankAccount bankAccount;//tip BankAccount (variabila bankAccount este de tipul clasei BankAccount);
+    // si clasele sunt tipuri in Java
+    private BankAccount [] bankAccounts;//tip array de BankAccount, nume field: bankAccounts
+
+    public BankAccount[] getBankAccounts() {
+        return bankAccounts;
+    }
+
+    public void setBankAccounts(BankAccount[] bankAccounts) {
+        this.bankAccounts = bankAccounts;
+    }
+
+    public Client(String name, String cnp, BankAccount bankAccount) {
+        this.name = name;
+        this.cnp = cnp;
+        this.bankAccount = bankAccount;
+    }
+
+    public Client(String name, String cnp) {
+        this.name = name;
+        this.cnp = cnp;
+        this.bankAccount = new BankAccount();
+    }
+
+    public Client() {
+        this.bankAccount = new BankAccount();
+    }
+
+    public String toString() { //creere metoda prin care in main prin System.out.println(client)/
+        // System.out.println(client.toString()) afiseaza ce este in metoda to string (nume, cnp, banckAccount;
+        return "Name= " + this.name + ", CNP=" + this.cnp + ", Bank account= " + this.bankAccount;
+
+    }// cu Alt+Ins se poate insera toString si suprascrie metoda de mai sus
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCnp() {
+        return cnp;
+    }
+
+    public void setCnp(String cnp) {
+        this.cnp = cnp;
+    }
+
+    public BankAccount getBankAccount() {
+        return bankAccount;
+    }
+
+    public void setBankAccount(BankAccount bankAccount) {
+        this.bankAccount = bankAccount;
+    }
+}
